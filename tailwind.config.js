c218ecba1441f200a3c5f47d59624a995a70d3c1
Andreas Gücklhorn <andreas@guecklhorn.com>
2024-11-02 11:14:45 +0100

    Change highlight color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     content: [],
     theme: {
         colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            white: "#ffffff",
-            black: "#030712",
-            gray: "#9ca3af",
-            muted: "#e5e7eb",
-            highlight: "#caa240",
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: '#ffffff',
+            black: '#030712',
+            gray: '#9ca3af',
+            muted: '#e5e7eb',
+            highlight: '#053577',
         },
         container: {
             center: true,
-            padding: "1rem",
+            padding: '1rem',
         },
         extend: {
             fontFamily: {
-                serif: ["RobotoSlab", ...defaultTheme.fontFamily.serif],
+                serif: ['RobotoSlab', ...defaultTheme.fontFamily.serif],
             },
         },
     },
-    plugins: [require("@tailwindcss/typography")],
-};
+    plugins: [require('@tailwindcss/typography')],
+}
